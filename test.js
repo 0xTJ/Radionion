57d1a7f6da4ed70a5a3d437a69e6a774308f7cbd
Thomas Jager <thomasj@thomasjager.ca>
2017-08-09 22:09:34 -0400

    Replaced spaces with tabs

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
 var internetradio = require('node-internet-radio');
 const exec = require('child_process').exec;
 
-exec('wget -O - http://streaming.radionomy.com/PassionLoveRadio | madplay  -', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-});
-
 var testStream = "http://streaming.radionomy.com/PassionLoveRadio";
+
+exec('wget -O - ' + testStream + ' | madplay -', (error, stdout, stderr) => {
+	if (error) {
+		console.error(`exec error: ${error}`);
+		return;
+	}
+	console.log(`stdout: ${stdout}`);
+	console.log(`stderr: ${stderr}`);
+});
+	
 internetradio.getStationInfo(testStream, function(error, station) {
-  console.log(station);
+	console.log(station);
 });

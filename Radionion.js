186a7ae4d2f3856f1ee572f5956fa987c1631c1f
Thomas Jager <thomasj@thomasjager.ca>
2017-08-09 22:14:58 -0400

    Removed sleep

--- Radionion.js
+++ Radionion.js
@@ -1,18 +1,16 @@
 var internetradio = require('node-internet-radio');
 //const exec = require('child_process').exec;
-var sleep = require('sleep');
 var oledExp = require("/usr/bin/node-oled-exp");
-const delay = require('delay');
 
 var streamURL = "http://streaming.radionomy.com/PassionLoveRadio";
 
-//exec('wget -O - ' + streamURL + ' | madplay  -', (error, stdout, stderr) => {
-//  if (error) {
-//    console.error(`exec error: ${error}`);
-//    return;
-//  }
-//  console.log(`stdout: ${stdout}`);
-//  console.log(`stderr: ${stderr}`);
+//exec('wget -O - ' + streamURL + ' | madplay	-', (error, stdout, stderr) => {
+//	if (error) {
+//		console.error(`exec error: ${error}`);
+//		return;
+//	}
+//	console.log(`stdout: ${stdout}`);
+//	console.log(`stderr: ${stderr}`);
 //});
 
 oledExp.init();
@@ -20,11 +18,10 @@ oledExp.setColumnAddressing(63,95);
 oledExp.scroll(0, 0, 0, 7);
 
 internetradio.getStationInfo(streamURL, function(error, station) {
-  oledExp.setTextColumns();
-  oledExp.setCursor(0, 5);
-  oledExp.write(station.title);
-  while(true) {
-    delay(1000);
-    oledExp.write(station.title);
-  }
+	oledExp.setTextColumns();
+	oledExp.setCursor(0, 5);
+	oledExp.write(station.title);
+	while(true) {
+		oledExp.write(station.title);
+	}
 });
